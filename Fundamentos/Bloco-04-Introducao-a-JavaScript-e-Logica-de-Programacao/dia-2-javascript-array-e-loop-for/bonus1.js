30be93939b6ad8f09d5edaa862a4f7200ff665ec
sergio3213 <sergio.n.m.jr@gmail.com>
2021-08-27 15:39:24 -0300

    ex4.x

--- Fundamentos/Bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/bonus1.js
+++ Fundamentos/Bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/bonus1.js
@@ -1,7 +1,7 @@
-let number=[5,4,9,2,1];
+let number=[5,4,9,2,1,20,3];
 
 let ut=0;
-for(let cont0=0;cont0<number.length-ut+3;cont0+=1){
+for(let cont0=0;cont0<number.length;cont0+=1){
        
     for(let cont=0;cont!=number.length-1;cont+=1){
         if(number[cont]>number[cont+1]){
@@ -10,7 +10,6 @@ for(let cont0=0;cont0<number.length-ut+3;cont0+=1){
             number[cont+1]=number[cont];
             number[cont]=aux;
                 
-            console.log(number);         
         }
 
             
@@ -18,6 +17,7 @@ for(let cont0=0;cont0<number.length-ut+3;cont0+=1){
     
     ut+=1;
 
-    console.log(); 
+    
 
 }
+console.log(number);
